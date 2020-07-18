f0fb721c8f38817598400f6a8a6d2617895f6a41
andy <frank87010217@gmail.com>
2020-07-19 01:48:24 +0800

    make it better

--- blog/model.py
+++ blog/model.py
@@ -37,7 +37,7 @@ class Post(db.Model):
     title = db.Column(db.Text, nullable=False)
     subtitle = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text(16777216), nullable=False)
+    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
