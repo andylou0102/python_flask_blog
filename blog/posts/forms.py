247fcfa14ceb9ab86ca89c49a6ed3323284b6511
andy <frank87010217@gmail.com>
2020-07-21 15:08:15 +0800

    second update

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
     title = StringField('標題', validators=[DataRequired(), Length(min=2, max=200)])
-    subtitle = StringField('副標題', validators=[DataRequired(), Length(min=2, max=200)])
+    subtitle = StringField('副標題', validators=[DataRequired(), Length(min=2, max=500)])
     content = TextAreaField('內容', validators=[DataRequired(), Length(min=10, max=5000)])
     submit = SubmitField('Post')
