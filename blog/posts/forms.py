c1fe94e76354005788bead48e8de1981ffaafb9c
andy <frank87010217@gmail.com>
2020-07-21 15:23:19 +0800

    second update

--- blog/posts/forms.py
+++ blog/posts/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, TextField
 from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
     title = StringField('標題', validators=[DataRequired(), Length(min=2, max=200)])
-    subtitle = StringField('副標題', validators=[DataRequired(), Length(min=2, max=500)])
+    subtitle = TextField('副標題', validators=[DataRequired(), Length(min=2, max=500)])
     content = TextAreaField('內容', validators=[DataRequired(), Length(min=10, max=5000)])
     submit = SubmitField('Post')
